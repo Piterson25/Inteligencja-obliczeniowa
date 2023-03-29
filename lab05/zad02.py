c199e7fa5d00663f0c1f0dff97eb26875914cd4e
Piterson25 <piotrmaszczak25@gmail.com>
2023-03-29 21:54:48 +0200

    zad02

--- lab05/zad02.py
+++ lab05/zad02.py
@@ -7,15 +7,12 @@ df = df.sort_values(by=['species'])
 (train_set, test_set) = train_test_split(df.values, train_size=0.7, random_state=278833)
 
 def classify_iris(sl, sw, pl, pw): 
-    if pl <= 2.5:
-        return("setosa")
-    elif pw <= 1.6:
-        if pl <= 4.8:
-            return("versicolor")
-        else:
-            return("virginica")
-    else: 
-        return("virginica")
+    if pl <= 2.0:
+        return "setosa"
+    elif pl > 5.0 and sl >= 4.5:
+        return "virginica"
+    else:
+        return "versicolor"
 
 good_predictions = 0 
 len = test_set.shape[0] 
