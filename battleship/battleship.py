892f73cf443cd60068a52e49ba529938961e8f87
Piterson25 <piotrmaszczak25@gmail.com>
2023-03-31 21:45:25 +0200

    Fitness func fixes

--- battleship/battleship.py
+++ battleship/battleship.py
@@ -1,5 +1,7 @@
-import pygad
 import numpy as np
+import pygad
+
+gene_space = [0, 1]
 
 board = []
 
@@ -14,89 +16,113 @@ with open('input6x6.txt') as f:
                 row.append(line[i])
         board.append(row)
 
-print(board)
+# print(board)
 
-col_edges = list(map(int, board[0][1:]))
-row_edges = list(map(int, [row[0] for row in board[1:]]))
+column_counts = list(map(int, board[0][1:]))
+row_counts = list(map(int, [row[0] for row in board[1:]]))
 
-gene_space = [0, 1]
+board_size = (len(board) - 1, len(board[0]) - 1)
 
-board_values = []
+print(column_counts)
+print(row_counts)
+
+start_positions = {}
+
 for i in range(1, len(board)):
-    row_values = []
     for j in range(1, len(board[0])):
-        if board[i][j] not in (0, 1):
-            board[i][j] = 0
-        row_values.append(int(board[i][j]))
-    board_values.append(row_values)
+        if board[i][j] == '0' or board[i][j] == '1':
+            start_positions[(j - 1, i - 1)] = int(board[i][j])
 
+print(start_positions)
 
-# print(col_edges)
-# print(row_edges)
-# print(board_values)
-
-
-def is_valid_ship(visited, rw, cl, ship_size):
-    for length in range(ship_size):
-        if rw + length >= len(visited) or cl + length >= len(visited[0]) or visited[rw + length][cl + length] == 1:
-            return False
-        if rw + length > 0 and cl + length > 0 and visited[rw + length - 1][cl + length - 1] == 1:
-            return False
-        if rw + length < len(visited) - 1 and cl + length > 0 and visited[rw + length + 1][cl + length - 1] == 1:
-            return False
-        if cl + length > 0 and visited[rw + length][cl + length - 1] == 1:
-            return False
-        if cl + length < len(visited[0]) - 1 and visited[rw + length][cl + length + 1] == 1:
-            return False
-    return True
+ship_lengths = [3, 2, 2, 1, 1, 1]
 
 
 def fitness_func(solution, solution_idx):
-    # Reshape solution into 2D grid
-    grid = np.reshape(solution, (len(row_edges), len(col_edges)))
+    # Konwertujemy rozwiązanie do postaci planszy o wymiarach 6x6.
+    sol_board = np.array(solution.reshape(board_size))
 
-    # Initialize score and visited grid
-    score = 0
-    visited = np.zeros((len(row_edges), len(col_edges)))
+    # Inicjalizujemy zmienne fitness oraz count_ships.
+    fitness = 0
+    count_ships = {3: 0, 2: 0, 1: 0}
 
-    empty_cells = 0
+    for position, value in start_positions.items():
+        row, col = position
+        if sol_board[col][row] != value:
+            fitness -= 10
 
-    # Check each ship in the solution
-    for ship_size in [3, 2, 2, 1, 1, 1]:
-        for i in range(len(row_edges)):
-            for j in range(len(col_edges)):
-                # Check if ship fits in grid starting at (i, j)
-                if i + ship_size <= len(row_edges):
-                    ship = grid[i:i + ship_size, j]
-                    if np.sum(ship) == ship_size and is_valid_ship(visited, i, j, ship_size):
-                        score += 1
-                        for k in range(ship_size):
-                            visited[i + k][j + k] = 1
-                if j + ship_size <= len(col_edges):
-                    ship = grid[i, j:j + ship_size]
-                    if np.sum(ship) == ship_size and is_valid_ship(visited, i, j, ship_size):
-                        score += 1
-                        for k in range(ship_size):
-                            visited[i][j + k] = 1
-                empty_cells = np.sum(visited == 0)
-                if empty_cells > 6:
-                    return score - empty_cells / (len(row_edges) * len(col_edges))
+    # Sprawdzamy ilość kawałków statków w rzędach.
+    for i in range(board_size[0]):
+        row_sum = np.sum(sol_board[i])
+        if row_sum == row_counts[i]:
+            fitness += 1
+        else:
+            fitness -= 1
 
-    return score - empty_cells / (len(row_edges) * len(col_edges))
+        # Sprawdzamy ilość kawałków statków w kolumnach.
+    for j in range(board_size[1]):
+        column_sum = np.sum(sol_board[:, j])
+        if column_sum == column_counts[j]:
+            fitness += 1
+        else:
+            fitness -= 1
+
+    # Sprawdzamy ilość statków o różnych długościach oraz ich pozycję na planszy.
+    for length in ship_lengths:
+        for i in range(board_size[0]):
+            for j in range(board_size[1]):
+                if sol_board[i, j] == 1:
+                    # Znaleziono początek statku.
+                    # Przeszukiwanie planszy w celu znalezienia pozostałych części tego samego statku.
+                    if length == 1:
+                        count_ships[length] += 1
+                    else:
+                        # Sprawdzamy poziomy statek o długości length.
+                        horizontal_check = True
+                        for k in range(1, length):
+                            if j + k >= board_size[1] or sol_board[i, j + k] == 0:
+                                horizontal_check = False
+                                break
+                        if horizontal_check:
+                            count_ships[length] += 1
+                            # Usuwamy znalezione części statku z planszy, żeby nie były liczone ponownie.
+                            sol_board[i, j:j + length] = 0
+                            continue
+
+                        # Sprawdzamy pionowy statek o długości length.
+                        vertical_check = True
+                        for k in range(1, length):
+                            if i + k >= board_size[0] or sol_board[i + k, j] == 0:
+                                vertical_check = False
+                                break
+                        if vertical_check:
+                            count_ships[length] += 1
+                            # Usuwamy znalezione części statku z planszy, żeby nie były liczone ponownie.
+                            sol_board[i:i + length, j] = 0
+
+    print(count_ships)
+
+    for ship_length, count in count_ships.items():
+        if ship_length == 3 and count > 1:
+            fitness -= 3
+        elif ship_length == 2 and count > 2:
+            fitness -= 2
+
+    return fitness
 
 
 fitness_function = fitness_func
 
 # ile chromsomów w populacji
 # ile genow ma chromosom
-sol_per_pop = 50
-num_genes = len(col_edges) * len(row_edges)
+sol_per_pop = 144
+num_genes = board_size[0] * board_size[1]
 
 # ile wylaniamy rodzicow do "rozmanazania" (okolo 50% populacji)
 # ile pokolen
 # ilu rodzicow zachowac (kilka procent)
 num_parents_mating = 20
-num_generations = 100
+num_generations = 1000
 keep_parents = 2
 
 # jaki typ selekcji rodzicow?
@@ -109,7 +135,7 @@ crossover_type = "single_point"
 # mutacja ma dzialac na ilu procent genow?
 # trzeba pamietac ile genow ma chromosom
 mutation_type = "random"
-mutation_percent_genes = 5
+mutation_percent_genes = 10
 
 # inicjacja algorytmu z powyzszymi parametrami wpisanymi w atrybuty
 ga_instance = pygad.GA(gene_space=gene_space,
@@ -133,17 +159,20 @@ print("Parameters of the best solution : {solution}".format(solution=solution))
 print("Fitness value of the best solution = {solution_fitness}".format(
     solution_fitness=solution_fitness))
 
-# tutaj dodatkowo wyswietlamy sume wskazana przez jedynki
-prediction = np.sum(solution)
-print("Predicted output based on the best solution : {prediction}".format(
-    prediction=prediction))
-
-solution_print = solution.reshape((len(col_edges), len(row_edges))).tolist()
+solution_print = solution.reshape((len(column_counts), len(row_counts))).tolist()
 
 # Wyświetlenie najlepszego rozwiązania
 print("Najlepsze rozwiązanie:")
 for row in solution_print:
     int_row = [int(x) for x in row]
     print(int_row)
+
+print("Prawidlowe ulozenie:")
+print([0, 0, 0, 0, 0, 1])
+print([0, 1, 1, 0, 0, 0])
+print([0, 0, 0, 0, 0, 0])
+print([1, 0, 0, 1, 0, 1])
+print([0, 0, 0, 0, 0, 1])
+print([1, 1, 0, 0, 0, 1])
 
 ga_instance.plot_fitness()
